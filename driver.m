765e6df44e53f934d39ce9a711696c23be811d9f
JetNoLi <hendricks.jet@gmail.com>
2021-06-30 23:30:42 +0200

    Added way to calculate average speedup

--- driver.m
+++ driver.m
@@ -14,12 +14,12 @@ encodedImg = imshow(encodedImage);
 
 time = zeros(20, 1);
 
-for j=1:20
+for j=1:21
   tic
-  for i=1:20 
+  for i=1:21 
     decodedMessage = char(decodeImage(encodedImage, size(toEncodeBefore, 2)));
   end
-  time(j) = toc * 1000 / 20;
+  time(j) = toc * 1000 / 21;
 end
 
 
@@ -30,8 +30,16 @@ encodedImage(1,1,:)
 encodedImage(1,2,:)
 encodedImage(1,3,:)
 
-plot(1:20, time)
+plot(1:21, time)
 title("Timing Chart for Decoding Golden Measure")
 xlabel("N")
 ylabel("Time [ms]")
+
+total = 0
+
+for i = 2:21
+  total = total + time(i)
+end
+
+speed = total/20
 
